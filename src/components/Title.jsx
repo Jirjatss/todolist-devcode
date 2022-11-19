4485fcb29636096258f07ccdd2b888706d5cc61b
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-11-19 08:04:36 +0800

    2

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -68,7 +68,7 @@ const Title = (props) => {
             </>
           )}
         </div>
-        <label className="btn bg-sky-500 hover:bg-sky-600 border-none gap-2 h-2 font-semibold text-base normal-case px-6 lg:px-5 rounded-full" type="button" data-cy={props.data_cy} onClick={props.function} htmlFor="mymodal2">
+        <label className="btn bg-sky-500 hover:bg-sky-600 border-none gap-2 h-2 font-semibold text-base normal-case px-6 lg:px-5 rounded-full" type="button" data-cy={props.data_cy} onClick={props.function}>
           <span>
             <Plus size={30} color={"white"} className="-mr-2" />
           </span>
