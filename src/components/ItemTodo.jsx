38b68bb5f5d330454c13ab2f50f1c0eff39e063a
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2022-11-19 20:44:33 +0800

    23

--- src/components/ItemTodo.jsx
+++ src/components/ItemTodo.jsx
@@ -34,14 +34,14 @@ const ItemTodo = (props, setData) => {
             </label>
           </div>
 
-          <input type="checkbox" id="deltodo" data-cy="modal-delete" className={modal ? "modal-toggle" : "hidden"} />
+          <input type="checkbox" id="deltodo" data-cy="todo-item-delete-button" className={modal ? "modal-toggle" : "hidden"} />
           <label htmlFor="deltodo" className="modal cursor-pointer" data-cy="todo-item-delete-button">
             <label className="modal-box relative bg-white" htmlFor="">
               <div className="text-center text-7xl mb-5">⚠️</div>
               <h3 className="text-lg text-center">Apakah anda yakin menghapus activity?</h3>
               <h3 className="font-bold text-lg text-center">"{props.title}"</h3>
               <div className="modal-action grid grid-cols-2 px-10">
-                <label htmlFor="deltodo" className="btn bg-slate-400 border-none hover:bg-slate-500 text-black rounded-full" data-cy="modal-delete-cancel-button">
+                <label onClick={() => setModal(false)} className="btn bg-slate-400 border-none hover:bg-slate-500 text-black rounded-full" data-cy="modal-delete-cancel-button">
                   Batal
                 </label>
                 <label htmlFor="infotodo" className="btn bg-red-500 border-none hover:bg-red-600 text-white rounded-full" onClick={() => props.del(props.id, setModal(false))} data-cy="modal-delete-confirm-button">
